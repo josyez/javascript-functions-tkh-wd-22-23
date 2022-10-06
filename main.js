61e86172d85dde95d30fe2ffc02b1a45dde6b410
Joselyn <cuzco.joselyn@gmail.com>
2022-10-05 20:05:31 -0400

    added js

---
+++ main.js
@@ -0,0 +1,12 @@
+function setDimension(height, width){
+    document.getElementById("container").style.height = height
+    document.getElementById("container").style.width = width
+    document.getElementById("container").style
+}
+setDimension("30vh", "80vw")
+
+const colorChange = () => {
+    document.getElementById("one").style.background = '#000'
+}
+
+colorChange()
